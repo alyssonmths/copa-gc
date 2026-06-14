609eab6b8248c03a2743606c5ad1f54fbc15724b
Jesus Miguel <jesusalmenares04@gmail.com>
2026-06-14 15:52:52 -0300

    feat(carga): cria CargaInicial com 8 selecoes

---
+++ src/util/CargaInicial.java
@@ -0,0 +1,30 @@
+package util;
+import model.Partida;
+import model.Selecao;
+import service.Copa;
+
+public class CargaInicial {
+    public static void carregar(Copa copa){
+        //GRUPO A
+        Selecao brasil= new Selecao("Brasil", 'A', "BRA");
+        Selecao argentina= new Selecao("Argentina", 'A', "ARG");
+        Selecao franca= new Selecao("França", 'A', "FRA");
+        Selecao alemanha= new Selecao("Alemanha", 'A', "ALE");
+
+        //GRUPO B
+        Selecao espanha= new Selecao("Espanha", 'B', "ESP");
+        Selecao portugal= new Selecao("Portugal", 'B', "POR");
+        Selecao inglaterra= new Selecao("Inglaterra",'B', "ING");
+        Selecao mexico= new Selecao("Mexico", 'B', "MEX");
+
+        copa.adicionarSelecoes(brasil);
+        copa.adicionarSelecoes(argentina);
+        copa.adicionarSelecoes(franca);
+        copa.adicionarSelecoes(alemanha);
+        copa.adicionarSelecoes(espanha);
+        copa.adicionarSelecoes(mexico);
+        copa.adicionarSelecoes(portugal);
+        copa.adicionarSelecoes(inglaterra);
+    }
+
+}
